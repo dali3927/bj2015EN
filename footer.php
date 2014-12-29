996cdbd7915d58380c30b60615728e3bc2857965
dali3927 <darrenli3927@gmail.com>
2014-12-29 17:11:27 +0800

    v1
    first version

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+
+// =============================================================================
+// FOOTER.PHP
+// -----------------------------------------------------------------------------
+// The site footer. Consists of the top footer area for widgets and the bottom
+// footer area for secondary information. Includes wp_footer() hook as well.
+//
+// Content is output based on which Stack has been selected in the Customizer.
+// To view and/or edit the markup of your Stack's index, first go to "views"
+// inside the "framework" subdirectory. Once inside, find your Stack's folder
+// and look for a file called "wp-footer.php," where you'll be able to find the
+// appropriate output.
+// =============================================================================
+
+?>
+<div style="display:none;"><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/zelnorm-cheapest-online.html">Can I get generic zelnorm Online Without Prescription in Canada</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/propecia-cheapest-online.html">Purchase cheap propecia Online Without Prescription in USA</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/periactin-cheapest-online.html">Sales of periactin pharmacy Walmart in Australia</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/femara-cheapest-online.html">Where can I Purchase femara No Prescription Over The Counter</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/prednisolone-cheapest-online.html">Purchase lowest price prednisolone Online No Prescription in UK</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/viagra-cheapest-online.html">Cost of viagra pills Walmart in UK</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/betnovate-cheapest-online.html">Discount generic betnovate pharmacy Price in USA</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/ortho-tri-cyclen-cheapest-online.html">Sales ortho-tri-cyclen tablets Over The Counter</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/fluoxetine-cheapest-online.html">Sales fluoxetine tablets in USA</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/viagra-soft-tabs-cheapest-online.html">Cheapest place to Buy viagra-soft-tabs No Prescription Needed in UK</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/aldactone-cheapest-online.html">Get a Prescription generic aldactone Online No Prescription in Australia</a><a href="http://beijing.thegmic.com/overnight-delivery-sales-purchase/viagra-professional-cheapest-online.html">Where to Buy viagra-professional Without Prescription Over The Counter</a></div>
+<?php x_get_view( x_get_stack(), 'wp', 'footer' ); ?>
